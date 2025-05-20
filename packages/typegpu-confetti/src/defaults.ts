ecb9216f7847a41e4f2d359f6675aa46a943b1a3
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2025-05-20 12:53:18 +0200

    feat: Web component (#6)

---
+++ packages/typegpu-confetti/src/defaults.ts
@@ -0,0 +1,46 @@
+import { randf } from '@typegpu/noise';
+import tgpu from 'typegpu';
+import * as d from 'typegpu/data';
+import { particles } from './schemas';
+import type { ConfettiPropTypes } from './types';
+
+const t = tgpu;
+
+export const defaults: {
+  [K in keyof ConfettiPropTypes]-?: NonNullable<ConfettiPropTypes[K]>;
+} = {
+  maxDurationTime: 2,
+  initParticleAmount: 200,
+  maxParticleAmount: 1000,
+
+  size: 1,
+  colorPalette: [
+    [154, 177, 155, 1],
+    [67, 129, 193, 1],
+    [99, 71, 77, 1],
+    [239, 121, 138, 1],
+    [255, 166, 48, 1],
+  ],
+
+  gravity: () => {
+    'kernel';
+    return d.vec2f(0, -0.3);
+  },
+
+  initParticle: ({ index: i }) => {
+    'kernel';
+    // @ts-ignore
+    const particle: d.Infer<typeof ParticleData> = particles.value[i];
+
+    particle.position = d.vec2f(
+      randf.sample() * 2 - 1,
+      randf.sample() / 1.5 + 1,
+    );
+    particle.velocity = d.vec2f(
+      randf.sample() * 2 - 1,
+      -(randf.sample() / 25 + 0.01) * 50,
+    );
+
+    particles.value[i] = particle;
+  },
+};
---
+++ packages/typegpu-confetti/src/utils.ts
@@ -0,0 +1,147 @@
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import type { AnyData, Infer } from 'typegpu/data';
+
+import type { TgpuBuffer, TgpuRoot } from 'typegpu';
+import { RootContext } from './context';
+
+export function useRoot(): TgpuRoot {
+  const root = useContext(RootContext);
+
+  if (root === null) {
+    throw new Error('No root (tgpu.init) object passed via context to the component.');
+  }
+  return root;
+}
+
+export function useBuffer<T extends AnyData>(
+  schema: T,
+  value?: Infer<T> | undefined,
+): TgpuBuffer<T> {
+  const root = useRoot();
+  const bufferRef = useRef<TgpuBuffer<T> | null>();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <don't recreate buffer on value change>
+  let buffer = useMemo(() => {
+    if (bufferRef.current) {
+      bufferRef.current.destroy();
+    }
+    const buffer = root.createBuffer(schema, value);
+    bufferRef.current = buffer;
+    return buffer;
+  }, [root, schema]);
+
+  // biome-ignore lint/style/noNonNullAssertion: <set in memo, strict mode acting weird>
+  buffer = bufferRef.current!;
+
+  useLayoutEffect(() => {
+    if (value !== undefined && buffer && !buffer.destroyed) {
+      buffer.write(value);
+    }
+  }, [value, buffer]);
+
+  const cleanupRef = useRef<ReturnType<typeof setTimeout> | null>();
+
+  useEffect(() => {
+    if (cleanupRef.current !== null) {
+      clearTimeout(cleanupRef.current);
+    }
+
+    return () => {
+      cleanupRef.current = setTimeout(() => {
+        buffer.destroy();
+      }, 1000);
+    };
+  }, [buffer]);
+
+  return buffer;
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: it's fine
+function useEvent<TFunction extends (...params: any[]) => any>(
+  handler: TFunction,
+) {
+  const handlerRef = useRef(handler);
+
+  useLayoutEffect(() => {
+    handlerRef.current = handler;
+  });
+
+  return useCallback((...args: Parameters<TFunction>) => {
+    const fn = handlerRef.current;
+    return fn(...args);
+  }, []) as TFunction;
+}
+
+export function useFrame(
+  loop: (deltaTime: number) => unknown,
+  isRunning = true,
+) {
+  const loopEvent = useEvent(loop);
+  useEffect(() => {
+    if (!isRunning) {
+      return;
+    }
+
+    let lastTime = Date.now();
+
+    const runner = () => {
+      const now = Date.now();
+      const dt = now - lastTime;
+      lastTime = now;
+      loopEvent(dt);
+      frame = requestAnimationFrame(runner);
+    };
+
+    let frame = requestAnimationFrame(runner);
+
+    return () => cancelAnimationFrame(frame);
+  }, [loopEvent, isRunning]);
+}
+
+interface DeviceContext {
+  device: GPUDevice;
+  adapter: GPUAdapter;
+}
+
+export const useDevice = (
+  adapterOptions?: GPURequestAdapterOptions,
+  deviceDescriptor?: GPUDeviceDescriptor,
+) => {
+  const [state, setState] = useState<DeviceContext | null>(null);
+
+  useEffect(() => {
+    setState(null); // resetting old adapter and device
+
+    let deviceContext: Promise<DeviceContext> | DeviceContext = (async () => {
+      const adapter = await navigator.gpu.requestAdapter(adapterOptions);
+      if (!adapter) {
+        throw new Error('No appropriate GPUAdapter found.');
+      }
+      const device = await adapter.requestDevice(deviceDescriptor);
+      if (!device) {
+        throw new Error('No appropriate GPUDevice found.');
+      }
+      deviceContext = { adapter, device };
+      setState(deviceContext);
+      return deviceContext;
+    })();
+
+    return () => {
+      if (deviceContext instanceof Promise) {
+        deviceContext.then((dev) => dev.device.destroy());
+      } else {
+        deviceContext.device.destroy();
+      }
+    };
+  }, [adapterOptions, deviceDescriptor]);
+
+  return { adapter: state?.adapter ?? null, device: state?.device ?? null };
+};
