1b7bbdead106be648b736645a6937b6c100bf6c5
Iwo Plaza <iwoplaza@gmail.com>
2026-05-12 22:18:20 +0200

    chore: Rename slots and accessors (#32)

--- packages/typegpu-confetti/src/index.ts
+++ packages/typegpu-confetti/src/index.ts
@@ -1,18 +1,41 @@
 export {
+  //
   // slots
-  canvasAspectRatio,
-  deltaTime,
-  // function shells
-  type GravityFn,
+  //
+  /** @deprecated Use `maxParticleAmountSlot` instead */
+  maxParticleAmount,
+  maxParticleAmount as maxParticleAmountSlot,
+  /** @deprecated Use `gravitySlot` instead */
   gravity,
+  gravity as gravitySlot,
+  /** @deprecated Use `initParticleSlot` instead */
+  initParticle,
+  initParticle as initParticleSlot,
+  /** @deprecated Use `maxDurationTimeSlot` instead */
+  maxDurationTime,
+  maxDurationTime as maxDurationTimeSlot,
+  //
+  // function shells
+  //
+  type GravityFn,
   gravityFn,
   type InitParticleFn,
-  initParticle,
   initParticleFn,
-  maxDurationTime,
-  maxParticleAmount,
+  //
+  // accessors
+  //
+  /** @deprecated Use `deltaTimeAccess` instead */
+  deltaTime,
+  deltaTime as deltaTimeAccess,
+  /** @deprecated Use `aspectRatioAccess` instead */
+  canvasAspectRatio,
+  canvasAspectRatio as aspectRatioAccess,
+  /** @deprecated Use `particlesAccess` instead */
   particles,
+  particles as particlesAccess,
+  /** @deprecated Use `timeAccess` instead */
   time,
+  time as timeAccess,
 } from './core/schemas.ts';
 
 export type { ConfettiProps, ConfettiRef } from './core/types.ts';
